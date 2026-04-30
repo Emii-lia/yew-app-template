08e5ed23d80f8076ca88abafc558560e6f8920b5
Emii-lia <fiaromiangaly@gmail.com>
2026-04-30 16:26:41 +0300

    refactor(components): using the new component instead of the deprecated function_component

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -5,10 +5,10 @@ mod not_found;
 pub(crate) use routes::*;
 pub(crate) use page::*;
 
-use yew::{function_component, html, Html};
+use yew::{component, html, Html};
 use yew_router::prelude::{BrowserRouter, Switch};
 
-#[function_component(App)]
+#[component(App)]
 pub fn app() -> Html {
   html! {
     <BrowserRouter>
